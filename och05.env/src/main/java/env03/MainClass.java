f9892a991cba9262fe4d145df0e8f1525e5ac458
시작 <78341551+start0ys@users.noreply.github.com>
2021-10-03 15:51:04 +0900

    환경설정

---
+++ och05.env/src/main/java/env03/MainClass.java
@@ -0,0 +1,22 @@
+package env03;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("1 Run");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		System.out.println("4. adminConfig Before Run");
+		AdminConnection connection = ctx.getBean("adminConfig",AdminConnection.class);
+		System.out.println("5 adminConfih After Run");
+		
+		System.out.println("connection.getAdminId adminID : " + connection.getAdminId());
+		System.out.println("connection.getAdminPw adminPW : " + connection.getAdminPw());
+		System.out.println("connection.getSub_adminId() sub_adminID : " + connection.getSub_adminId());
+		System.out.println("connection.getSub_adminPw() sub_adminPW : " + connection.getSub_adminPw());
+		ctx.close();
+
+	}
+
+}
